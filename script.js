fb37db824e40d374fe17627cea571dcf0dba2dd2
nickqadosh <nickqadosh@gmail.com>
2020-08-22 09:49:12 +0100

    exemplaire 1 du projet

---
+++ script.js
@@ -0,0 +1,25 @@
+fetch("http://cross-origin.herokuapp.com/https://www.gabonreview.com/feed/")
+  .then((response) => response.text())
+  .then((str) => new window.DOMParser().parseFromString(str, "text/xml"))
+  .then((data) => {
+    console.log(data);
+    const items = data.querySelectorAll("item");
+    let html = ``;
+    items.forEach((el) => {
+      html += `
+        <article>
+          <img src="${
+            el.querySelector("link").innerHTML
+          }/image/large.png" alt="">
+          <h2>
+            <a href="${
+              el.querySelector("link").innerHTML
+            }" target="_blank" rel="noopener">
+              ${el.querySelector("title").innerHTML}
+            </a>
+          </h2>
+        </article>
+      `;
+    });
+    document.body.insertAdjacentHTML("beforeend", html);
+  });
